3d25f3b8f911d9ae029e67235440f5ab9db0b24a
DeveloperDulli <dulli1616@kakao.com>
2021-08-04 14:22:20 +0900

    상속

--- inherit/src/inherit/Bird.java
+++
@@ -1,9 +0,0 @@
-package inherit;
-
-public abstract class Bird {
-	public abstract void sing();
-	// abstract 추상클래스
-	public void fly() {
-		System.out.println("날다.");
-	}
-}
--- inherit/src/inherit/BirdExam.java
+++
@@ -1,8 +0,0 @@
-package inherit;
-
-public class BirdExam {
-	public static void main(String[] args) {
-		
-	}
-
-}
