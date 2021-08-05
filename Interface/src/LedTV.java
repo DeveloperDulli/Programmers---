8587196d5a22618378053a00951c91aa04e52011
DeveloperDulli <dulli1616@kakao.com>
2021-08-05 14:22:56 +0900

    인터페이스

---
+++ Interface/src/LedTV.java
@@ -0,0 +1,30 @@
+
+public class LedTV implements TV {
+
+	@Override
+	public void turnOn() {
+		System.out.println("켜다");
+
+	}
+
+	@Override
+	public void turnOff() {
+		System.out.println("끄다");
+		
+
+	}
+
+	@Override
+	public void changeVolume(int volume) {
+		System.out.println("볼륨을 조정하다");
+
+	}
+
+	@Override
+	public void changeChannel(int channel) {
+		System.out.println("채널을 지정하다");
+		
+
+	}
+
+}
